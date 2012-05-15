cf7e75fab7936edbe9751b9af66d6b2191f49085
Manuel Vögele <mvoegele@arcor.de>
2012-05-16 00:22:04 +0200

    indexing fixed javadoc for makeClone()

--- src/de/dustboystudios/utils/cloner/Cloner.java
+++ src/de/dustboystudios/utils/cloner/Cloner.java
@@ -1,5 +1,8 @@
 package de.dustboystudios.utils.cloner;
 
+import java.lang.reflect.Field;
+import java.util.Set;
+
 /**
  * <p>A class containing functionality for cloning any kind of classes.
  * Since this cloner uses reflection you should prefer using the clone method of the class if available.</p>
@@ -12,9 +15,11 @@ package de.dustboystudios.utils.cloner;
 public class Cloner<T>
 {
 	/**
-	 * Indicates whether only a single clone should be made
+	 * A reference object to create the clones from
 	 */
-	private boolean singleInstance;
+	private T referenceObject;
+	
+	private Set<Reference> objects;
 	
 	/**
 	 * <p>Instantiates and prepares a new Cloner instance for cloning.
@@ -23,8 +28,9 @@ public class Cloner<T>
 	 * <p>Only use this if you want to create multiple clones of an object.
 	 * If you want to make only one clone better use the static clone method since its faster.</p>
 	 * @param object The object which should be cloned
+	 * @throws IllegalAccessException if there is a problem when accessing a field
 	 */
-	public Cloner(T object)
+	public Cloner(T object) throws IllegalAccessException
 	{
 		this(object, false);
 	}
@@ -34,15 +40,44 @@ public class Cloner<T>
 	 * This constructor is for internal use only.</p>
 	 * @param object The object which should be cloned 
 	 * @param singleInstance if only one instance of the cloned object should be created
+	 * @throws IllegalAccessException if there is a problem when accessing a field
 	 */
-	private Cloner(T object, boolean singleInstance)
+	private Cloner(T object, boolean singleInstance) throws IllegalAccessException
 	{
-		this.singleInstance = singleInstance;
+		referenceObject = object;
+		index(object);
+		if (!singleInstance) {
+			referenceObject = makeClone();
+		}
 	}
 	
 	/**
-	 * Returns a copy of the instance passed
-	 * @return A clone which 
+	 * Adds the specified object and all contained objects to the index
+	 * 
+	 * @param object the object to index
+	 * @throws IllegalAccessException if there is a problem when accessing a field
+	 */
+	public void index(Object object) throws IllegalAccessException
+	{
+		Reference ref = new Reference(object);
+		if (!objects.contains(ref))
+		{
+			objects.add(ref);
+			for (Field field : object.getClass().getFields())
+			{
+				field.setAccessible(true);
+				if (!field.getType().isPrimitive()) {
+					index(field.get(object));
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Returns a copy of the instance passed to the constructor.
+	 * The clone will have the state the instance had when it was passed to the constructor.
+	 * 
+	 * @return the clone
 	 */
 	public T makeClone()
 	{
@@ -53,8 +88,9 @@ public class Cloner<T>
 	 * Clones the specified object
 	 * @param object the object to clone
 	 * @return an exact copy of the specified object which is completely independent from the others
+	 * @throws IllegalAccessException if there is a problem when accessing a field
 	 */
-	public static <T> T clone(T object)
+	public static <T> T clone(T object) throws IllegalAccessException
 	{
 		Cloner<T> cloner = new Cloner<T>(object);
 		return cloner.makeClone();
