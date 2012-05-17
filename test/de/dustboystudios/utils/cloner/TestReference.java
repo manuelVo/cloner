5e4c5f1d9c42eb5859788f9f25f868f56ea6fa60
Manuel Vögele <mvoegele@arcor.de>
2012-05-18 00:37:14 +0200

    Test class for Reference

---
+++ test/de/dustboystudios/utils/cloner/TestReference.java
@@ -0,0 +1,19 @@
+package de.dustboystudios.utils.cloner;
+
+import junit.framework.TestCase;
+
+public class TestReference extends TestCase
+{
+	public void testEquals()
+	{
+		Object o1 = new Object();
+		Object o2 = new Object();
+		Reference same1 = new Reference(o1);
+		Reference same2 = new Reference(o1);
+		Reference different = new Reference(o2);
+		assertSame(same1.getObject(), same2.getObject());
+		assertNotSame(same1.getObject(), different.getObject());
+		assertEquals(same1, same2);
+		assertFalse(same1.equals(different));
+	}
+}
