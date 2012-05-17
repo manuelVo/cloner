c0d1ce08330b6e8d596401b8545b478b08c4b519
Manuel Vögele <mvoegele@arcor.de>
2012-05-18 00:51:38 +0200

    Method stubs for cloner test

---
+++ test/de/dustboystudios/utils/cloner/TestCloner.java
@@ -0,0 +1,35 @@
+package de.dustboystudios.utils.cloner;
+
+import junit.framework.TestCase;
+
+/**
+ * Testclass for {@link Cloner}
+ *
+ * @author Manuel Vögele
+ */
+public class TestCloner extends TestCase
+{
+	/**
+	 * A test for simple cloning covering any types of data only generating a single clone
+	 */
+	public static void testSingleClone()
+	{
+		fail("Not implemented yet!");
+	}
+	
+	/**
+	 * A test for simple cloning generating multiple clones
+	 */
+	public static void testMultiClone()
+	{
+		fail("Not implemented yet!");
+	}
+	
+	/**
+	 * A test for cloning objects with a ring closure in the class structure
+	 */
+	public static void testRingClosure()
+	{
+		fail("Not implemented yet!");
+	}
+}
