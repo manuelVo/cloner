0f20c97b4a5c5871c7836d966d0d32391215b345
Manuel Vögele <mvoegele@arcor.de>
2012-05-20 12:28:28 +0200

    A new TestclassObjectholder

---
+++ test/de/dustboystudios/utils/cloner/TestclassObjectholder.java
@@ -0,0 +1,47 @@
+package de.dustboystudios.utils.cloner;
+
+/**
+ * A class able to hold an object
+ *
+ * @param <T> the type of the object to hold
+ *
+ * @author Manuel Vögele
+ */
+public class TestclassObjectholder<T>
+{
+	private T object;
+	
+	/**
+	 * Initializes a new instance setting no values
+	 */
+	public TestclassObjectholder()
+	{
+	}
+	
+	/**
+	 * Initializes a new instance
+	 * @param object the object to hold
+	 */
+	public TestclassObjectholder(T object)
+	{
+		this.object = object;
+	}
+	
+	/**
+	 * Set a new object to hold
+	 * @param object the new object to hold
+	 */
+	public void setObject(T object)
+	{
+		this.object = object;
+	}
+	
+	/**
+	 * Returns the held object
+	 * @return the held object
+	 */
+	public T getObject()
+	{
+		return object;
+	}
+}
